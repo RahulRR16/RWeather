d550164b452138aeb054ead5d39287957e21b9d3
Rahul <rahulrr161996@gmail.com>
2025-01-29 21:12:03 +0000

    Initial Commit

---
+++ RWeather/RWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RWeatherApp.swift
+//  RWeather
+//
+//  Created by Rahul on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
